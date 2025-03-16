f8d766f434bec3187fc40abbea5f9cbcf7614adc
Srivathsan <srivprasanna@gmail.com>
2025-03-16 13:05:36 -0400

    type check fix #1

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,11 +1,11 @@
 import { Button } from "@/components/ui/button";
-import router from "next/router";
+import { redirect } from "next/navigation";
 
 export default function Page() {
     return (
         <>
             <p>ERROR, EITHER UR PASSWORD/EMAIL COMBINATION IS WRONG OR THE USER WITH THAT TITLE ALREADY EXISTS</p>
-            <Button variant="default" onClick={() => router.push('/login')}>Return to Login</Button>
+            <Button variant="default" onClick={() => redirect('/login')}>Return to Login</Button>
         </>
     )
 }
